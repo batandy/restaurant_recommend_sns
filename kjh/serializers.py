d3ce03b99b47dc843a51b4f3b9f908c51d390ac9
99apollo <99apollo@naver.com>
2023-01-06 18:39:03 +0900

    Revert "DB 웹이미지 확인2"
    This reverts commit f93b3fb5c0a88eb23c4ca686c7cddaa686a2c0f3.

--- kjh/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import restaurant
-
-class TestDataSerializer(ModelSerializer):
-    class Meta:
-        model = restaurant
-        fields = '__all__'
