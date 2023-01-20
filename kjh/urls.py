e884e53004405e72e089c297be8c4a5a750e5305
김민석 <batandy@naver.com>
2023-01-20 18:47:52 +0900

    앱 통일화

--- kjh/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='main'
-
-urlpatterns = [
-    path('a/', views.store, name='store'),
-    path('test/',views.restaurant_view,name='dbview'),
-]
