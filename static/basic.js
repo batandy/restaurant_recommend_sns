e44f5e911d5870c5f57657e2851af28625622709
김민석 <batandy@naver.com>
2023-01-23 19:43:54 +0900

    초기 화면 비율 수정 및 geolocation 기능 추가

--- static/basic.js
+++ static/basic.js
@@ -1,12 +1,24 @@
 var src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=fxqxlpqj6o"
 
 
-var mapDiv = document.getElementById('map');
-var map = new naver.maps.Map('map', {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
-});
-var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(37.3595704, 127.105399),
-    map: map
-});
+function onGeoSuccess(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live it", lat, lng);
+
+    var mapDiv = document.getElementById('map');
+    var map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(lat, lng),
+        zoom: 17
+    });
+    var marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(lat, lng),
+        map: map
+    });
+}
+function onGeoError(){
+    alert("Can't find you. No locate for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+
