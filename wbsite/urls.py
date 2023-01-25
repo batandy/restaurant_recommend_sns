e92a14a64a1bf3cebf4689a4f70626a4b96c8a60
seungwon <herbysw@naver.com>
2023-01-25 18:57:44 +0900

    로그인 기능 수정1

--- wbsite/urls.py
+++ wbsite/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main.views.index, name='index'),
     path('main/', include('main.urls')),
-    path('login/',include('login.urls')),
+    #path('login/',include('login.urls')),
 ]
