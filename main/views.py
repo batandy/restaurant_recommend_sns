687cdb1adcab511b48c555bbbcc8afc0898b3aa0
seungwon <herbysw@naver.com>
2023-01-28 11:39:09 +0900

    회원가입 추가

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 from .models import restaurants
 from django.core import serializers
+from django.contrib.auth import authenticate, login
+from main.forms import UserForm
 import json
 # Create your views here.
 from django.http import HttpResponse
@@ -16,6 +18,7 @@ def index(request):
     return render(request,"base.html",{'restaurant_db':restaurant_db})
 
 
+
 def store(request):
     return render(request,"main/storeinfo.html")
     
@@ -28,3 +31,17 @@ def restaurant_view(request):
 def getdata(request):
     restaurant_db = serializers.serialize("json", restaurants.objects.all())
     return HttpResponse(restaurant_db,content_type="text/json-comment-filtered")
+
+def signup(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'main/signup.html', {'form': form})
--- wbsite/settings.py
+++ wbsite/settings.py
@@ -139,5 +139,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# 로그인 성공후 이동하는 URL
+# 로그인,아웃 성공후 이동하는 URL
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
