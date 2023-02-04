c55e8da223bd79d3d587af33a00a923dcfc81252
seungwon <herbysw@naver.com>
2023-02-04 17:10:31 +0900

    프로필 연동1

--- main/urls.py
+++ main/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('mypage/', views.mypage, name='mypage'),
 ]
