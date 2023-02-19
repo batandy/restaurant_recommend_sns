7ebab2c5948a9edb6ba87df0f69c563dd080f24f
김민석 <batandy@naver.com>
2023-02-19 23:12:02 +0900

    데이터 수정

--- static/main.js
+++ static/main.js
@@ -2,19 +2,23 @@
 import fetchData from "./basic.js";
 
 const productsContainer = document.getElementById('divstores');
+console.log(productsContainer)
 // const location = JSON.parse(sessionStorage.getItem("location"));
-const datas_name=[]
-const datas_x=[]
-const datas_y=[]
+const data_name=[]
+const data_x=[]
+const data_y=[]
 const datas=[]
-
-
+const data_add=[]
+const data_num=[]
 
 fetchData().then((result) => {  //basic.js로 넘어온 데이터 가공 후 띄우기
+    console.log(result)
     for (var i = 0; i < result.length; i++) {
-        datas_x.push(result[i].fields.x)
-        datas_y.push(result[i].fields.y)
-        datas_name.push(result[i].fields.name)
+        data_x.push(result[i].fields.x)
+        data_y.push(result[i].fields.y)
+        data_name.push(result[i].fields.name)
+        data_add.push(result[i].market_address)
+        data_num.push(result[i].market_number)
     }
 
     let lat;
@@ -31,11 +35,11 @@ fetchData().then((result) => {  //basic.js로 넘어온 데이터 가공 후 띄
     }
 
     for (var i=0; i<result.length;i++){                    //현재 위치기준으로 매장선별
-        if(lat-0.05<=datas_x[i]&&datas_x[i]<=lat+0.050 && lng-0.050 <= datas_y[i] && datas_y[i] <= lng+0.050){
+        if(lat-0.05<=data_x[i]&&data_x[i]<=lat+0.050 && lng-0.050 <= data_y[i] && data_y[i] <= lng+0.050){
             const dataset = {
-                name: datas_name[i],
-                lat: datas_x[i],
-                lng: datas_y[i],
+                name: data_name[i],
+                lat: data_x[i],
+                lng: data_y[i],
             };
             datas.push(dataset);
         }
@@ -48,16 +52,6 @@ fetchData().then((result) => {  //basic.js로 넘어온 데이터 가공 후 띄
         productsContainer.appendChild(name);
 
         const latlng = document.createElement('p');
-        // naver.maps.Service.reverseGeocode({                   //위치변환인데 너무 오래걸림
-        //     coords: new naver.maps.LatLng(data.lat, data.lng),
-        // }, function(status, response) {
-        //     var result = response.v2, // 검색 결과의 컨테이너
-        //         address = result.address; // 검색 결과로 만든 주소
-        //         latlng.textContent = address.jibunAddress;
-        //         console.log(latlng)
-        //         productsContainer.appendChild(latlng);
-        //     }
-        // );
         latlng.textContent = `${data.lat}, ${data.lng}`;
         productsContainer.appendChild(latlng);
 
