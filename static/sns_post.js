d6ebf11d8f31ae8d9bbef2808cc99b4f5eea28d6
99apollo <99apollo@naver.com>
2023-02-24 18:49:49 +0900

    게시물 위치 추가

---
+++ static/sns_post.js
@@ -0,0 +1,20 @@
+
+let lat=0;
+let lng=0;
+
+if(sessionStorage.getItem("location")){                                 //위치확인
+                    const location = JSON.parse(sessionStorage.getItem("location"));
+                    lat=location.lat;
+                    lng=location.lng
+                }else{
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        lat = position.coords.latitude;
+                        lng = position.coords.longitude;
+                    });
+                }
+console.log(lat,lng);
+
+var mylat=document.getElementById("my-lat");
+var mylng=document.getElementById("my-lng");
+mylat.textContent = lat;
+mylng.textContent = lng;
